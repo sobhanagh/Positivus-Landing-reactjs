6e1099d46c53b6c3d4a5012512b9049cffe94c70
sobhan <sobhanaghajani893@gmail.com>
2024-08-25 17:18:53 +0330

    fix services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -135,7 +135,7 @@ const Services = () => {
                             <article key={card.title} className={`${card.bgColor} rounded-lg flex justify-between p-5 border-b-4 border-black`}>
                                 <div className="flex flex-col justify-between ">
                                     <h2 className={`${card.title.bgColor} ${card.title.txtColor} px-2 py-1 font-bold rounded-md lg:text-xl max-w-40`}>{card.title.name}</h2>
-                                    <div className="lex items-center gap-3">
+                                    <div className="flex items-center gap-3">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-7 lg:size-8 p-1 cursor-pointer rounded-full -rotate-45 hover:rotate-0 transition-all duration-200 ${card.btn?.bgColor} ${card.btn?.txtColor}`}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                                         </svg>
